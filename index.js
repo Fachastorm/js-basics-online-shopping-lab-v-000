b4561178ff4555cb5a759f893f3491739f39e29c
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2018-02-19 17:49:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,40 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  const price = Math.floor(Math.random() * 100) + 1;
+
+  cart.push({ [item]: price});
+
+  console.log(`${item} has been added to your cart.`);
+
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  const l = cart.length;
+
+  if (!l) {
+    return console.log(`Your shopping cart is empty`);
+  }
+
+  let itemsAndPrices = [];
+
+  for (let i = 0; i < l; i++) {
+    let itemAndPrice = cart[i];
+    let item = Object.keys(itemAndPrice)[0];
+    let price = itemAndPrice[item];
+
+    itemsAndPrices.push(` ${item} at \$${price}`);
+  }
+  switch(itemsAndPrices.length) {
+    case 1:
+    break;
+    case 2:
+    itemsAndPrices = itemsAndPrices.join(" and ");
+    break;
+    default:
+      itemsAndPrices
+  }
 }
 
 function total() {
